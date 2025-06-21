c3175be3ca62f360c6351f7ba5d1456869d6efe1
C4W Design <c4wdesign@gmail.com>
2025-06-22 00:41:24 +0200

    Moved server.js to root and updated imports

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
-import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
